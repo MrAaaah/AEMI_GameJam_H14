74b1286dbff5e440d7115312e9f14327b6249b63
MrAaaah <jules.minier@mraaaah.fr>
2014-03-09 00:20:03 -0500

    Intégration des sons dans le soundfx manager

--- Assets/Scripts/Managers/SoundFXManager.cs
+++ Assets/Scripts/Managers/SoundFXManager.cs
@@ -5,6 +5,28 @@ using System.Collections.Generic;
 public enum SoundFX {
 	OpenDoors,
 	DoorsActivated,
+
+	Hit,
+
+	WeaponSwing,
+//	WeaponKnifes,
+//	WeaponKunais,
+	Spit,
+//	Lazor,
+//	DoomDevice,
+
+	Die, 
+	GetResource,
+
+	HitResource,
+
+	FinPeriodeNonLethale,
+
+	HoverGUI,
+	SelectElementGUI,
+
+	TimerBeforeReturnGame,
+//	AlarmingTimerBeforeReturnGame,
 }
 
 [AddComponentMenu("Singletons/SoundFXManager")]
@@ -32,6 +54,19 @@ public class SoundFXManager : SingletonMonoBehaviour<SoundFXManager> {
 	private void LoadSounds () {
 		sounds.Add(SoundFX.OpenDoors, Resources.Load("Sounds/SFX015") as AudioClip);
 		sounds.Add(SoundFX.DoorsActivated, Resources.Load("Sounds/SFX014") as AudioClip);
+		sounds.Add(SoundFX.Hit, Resources.Load("Sounds/SFX005") as AudioClip);
+		sounds.Add(SoundFX.WeaponSwing, Resources.Load("Sounds/SFX004") as AudioClip);
+		sounds.Add(SoundFX.Spit, Resources.Load("Sounds/SFX008") as AudioClip);
+		
+		sounds.Add(SoundFX.Die, Resources.Load("Sounds/SFX011") as AudioClip);
+		sounds.Add(SoundFX.GetResource, Resources.Load("Sounds/SFX012") as AudioClip);
+		
+		sounds.Add(SoundFX.HitResource, Resources.Load("Sounds/SFX013") as AudioClip);
+		sounds.Add(SoundFX.FinPeriodeNonLethale, Resources.Load("Sounds/SFX016") as AudioClip);
+		sounds.Add(SoundFX.HoverGUI, Resources.Load("Sounds/SFX019") as AudioClip);
+		sounds.Add(SoundFX.SelectElementGUI, Resources.Load("Sounds/SFX020") as AudioClip);
+		sounds.Add(SoundFX.TimerBeforeReturnGame, Resources.Load("Sounds/SFX021") as AudioClip);
+
 		initialized = true;	
 	}
 	
