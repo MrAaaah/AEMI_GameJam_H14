d7a07b8b97a77c5d61d81b92f457dd394ae46b54
Martin Lavoie <broemartino@gmail.com>
2014-03-09 12:24:00 -0400

    Over ui

--- Assets/Scripts/Managers/mapManager.cs
+++ Assets/Scripts/Managers/mapManager.cs
@@ -134,8 +134,8 @@ public class mapManager
 
 				}
 				spawned = true;
-				GameObject.Find ("HUDPlayer1").GetComponent<OverGUI> ().setCharacter (players [1]);
-				GameObject.Find ("HUDPlayer2").GetComponent<OverGUI> ().setCharacter (players [0]);
+				GameObject.Find ("OverGUI1").GetComponent<OverGUI> ().setCharacter (players [1]);
+		GameObject.Find ("OverGUI2").GetComponent<OverGUI> ().setCharacter (players [0]);
 		}
 
 		private void destroyPlayer (int  i)
