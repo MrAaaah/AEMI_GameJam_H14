16f56b4cdb8bf902520c2a235c6f22468d420862
MrAaaah <jules.minier@mraaaah.fr>
2014-03-09 12:57:26 -0400

    - Suppression du bouton donnat l'acces au menu de craft - Enlevement de l'affichage du timer

--- Assets/Scripts/GameStates/Game.cs
+++ Assets/Scripts/GameStates/Game.cs
@@ -19,9 +19,6 @@ public class Game : State<GameState>
 	
 	public override void UpdateStateGUI ()
 	{
-		if (GUI.Button(new Rect(30, 130, 150, 30), "Prees to kill the dwarf")) {
-			GameStateManager.singleton.SetGameState (GameState.CraftScreen);
-		}
 	}
 	
 	public override void ExitState ()
