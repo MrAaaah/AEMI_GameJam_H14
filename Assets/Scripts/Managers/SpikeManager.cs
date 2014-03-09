a64dfbc8394079f3681396be25db4daf3fb45bb6
Martin Lavoie <broemartino@gmail.com>
2014-03-09 04:04:23 -0400

    Spike modif

--- Assets/Scripts/Managers/SpikeManager.cs
+++ Assets/Scripts/Managers/SpikeManager.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class SpikeManager : MonoBehaviour
 {
 
-		void OnTriggerEnter2D (Collider2D other)
+		void OnTriggerEnter (Collider other)
 		{
 				string layer = LayerMask.LayerToName (other.gameObject.layer);
 				if (layer.StartsWith ("Player")) {
